022afc10c9c862dfc16aa199ba7a0bd6110116b4
melvinalmonte <melvinalmonte@gmail.com>
2019-04-25 09:28:53 -0400

    added second method how to reverse an array

--- reverseArray/reversearray.py
+++ reverseArray/reversearray.py
@@ -1,2 +1,13 @@
-def reverseArray(a):
+# Using Extended Slice
+def reverse_array_slice(a):
     return a[::-1]
+
+
+# Appending a second array
+def reverse_array_second_array(a):
+    counter = len(a) - 1
+    tempArr = []
+    for i in range(len(a)):
+        tempArr.append(a[counter])
+        counter -= 1
+    return tempArr
